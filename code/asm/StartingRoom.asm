9a4fedec740db616705e90f8394f992cf54af89e
ShadowOne333 <shadow_one666@hotmail.com>
2024-10-29 13:37:37 -0600

    Restore Starting Room

--- code/asm/StartingRoom.asm
+++ code/asm/StartingRoom.asm
@@ -6,14 +6,14 @@ startingRoomasm:
 	ldr     r0,=AreaID
 	mov     r1,5			; new area
 	strb    r1,[r0]
-	mov     r1,0x25			; new door, 0x38
+	mov     r1,0x38			; new door, 0x38
 	strb    r1,[r0,2h]
 	ldr     r0,=HideHudFlag
 	mov     r1,0
 	strh    r1,[r0]
 	ldr     r0,=0x3000C75
 	strb    r1,[r0]
-	mov     r0,0x17			; music track ; E
+	mov     r0,0x0E			; music track ; E
 	bl      0x80039F4
 	b       0x8060FDA
 	.pool
